4b00dc984dd4618fc382af1672225df7f418d8c7
normalJaylan <915459957@qq.com>
2016-07-29 11:33:43 +0800

    老鼠走迷宫

---
+++ maze/main.cpp
@@ -0,0 +1,89 @@
+#define SIZE 15
+
+#include <iostream>
+#include <stdlib.h>
+#include "struct.h"
+using namespace std;
+
+Struct *move(int x, int y);
+void print();
+void start();
+
+int maze[SIZE][SIZE] = {0};         //-1: 走不通; 0: 不能走; 1: 可以走; 2: 已走过
+Struct *head = new Struct();
+
+void createMaze() {
+    maze[0][0] = maze[1][0] = maze[1][1] = maze[2][1] = maze[3][1] = maze[4][1] = maze[5][1] = maze[5][2] = maze[5][3] = maze[5][4] =
+    maze[5][5] = maze [5][6] = maze[5][7] = maze[4][7] = maze[3][7] = maze[3][8] = maze[3][9] = maze[3][10] = maze[3][11] = maze[4][11] = 
+    maze[5][11] = maze[6][11] = maze[7][11] = maze[8][11] = maze[9][11] = maze[9][10] = maze[9][9] = maze[9][8] = maze[9][7] = maze[9][6] = 
+    maze[9][5] = maze[10][5] = maze[11][5] = maze[12][5] = maze[13][5] = maze[13][6] = maze[14][6] = maze[14][7] = maze[14][8] = maze[14][9] =
+    maze[14][10] = maze[13][10] = maze[12][10] = maze[12][11] = maze[12][12] = maze[13][12] = maze[13][13] = maze[13][14] = maze[14][14] = 1;
+}
+
+void print() {
+    for (int i(0);i<SIZE;i++) {
+        for (int j(0);j<SIZE;j++) {
+            if (maze[i][j] == 0) 
+                cout << " 1";
+            else 
+                cout << "  ";
+        }
+        cout << "" << endl;
+    }
+}
+
+void start() {
+    head->x = 0;
+    head->y = 0;
+    head->next = NULL;
+
+    head = move(head->x, head->y);
+
+    while (true) {
+       head =  move(head->x, head->y);
+       if (head == NULL) {                                      //没有通路,退出程序
+           cout << "迷宫没有通路..." << endl;
+           exit(0);
+       }
+       if ((head->x == SIZE - 1) && (head->y == SIZE -1)) {     //找到通路,打印路线
+        //    print(); 
+       }
+    }    
+}
+
+
+
+Struct *move(int x, int y) {
+    int a, b;
+    a = b = 15;
+    if ((y + 1) < SIZE && maze[x][y+1] == 1) {            //选择下一步方向,优先选择右下
+        a = x;
+        b = y + 1;
+    }
+    else if ((x + 1) < SIZE && maze[x+1][y] == 1) {
+        a = x + 1;
+        b = y;
+    }
+    else if ((y -1) > -1 && maze[x][y-1]) {
+        a = x;
+        b = y - 1;
+    }
+    else if ((x - 1) > -1 && maze[x-1][y]) {
+        a = x - 1;
+        b = y;
+    }
+    if (a == 15 && b == 15) {                         //无路可走,需要回退
+        maze[x][y] = -1;
+        head = head->remove(head);
+    } else {
+        maze[x][y] = 2;
+        head = head->add(a, b, head);                 //有路可走,坐标入栈
+    }
+    return head;
+}
+
+int main() {
+    createMaze();
+
+ //   print();
+}
---
+++ maze/struct.h
@@ -0,0 +1,35 @@
+#define MAX 50
+
+#include <iostream>
+using namespace std;
+
+class Struct {
+    public :
+        Struct *next;
+        int x;
+        int y;
+    public :
+        Struct();
+        Struct *add(int x, int y, Struct *head);
+        Struct *remove(Struct *head);
+};
+
+Struct::Struct() {
+    x = y = 0;
+}
+
+Struct *Struct::add(int x, int y, Struct *head) {
+    Struct *node = new Struct();
+    node->next = head;
+    head = node;
+    return head;
+}
+
+Struct *Struct::remove(Struct *head) {
+    Struct *temp = head;
+    head = head->next;
+    delete(temp);
+    return head;
+}
+
+
