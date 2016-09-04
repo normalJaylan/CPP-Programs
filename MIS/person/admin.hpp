1c030e87211e0aacf051d9f8cc51e6e40e9127e7
normalJaylan <915459957@qq.com>
2016-09-05 00:44:24 +0800

    管理员类设计

---
+++ MIS/person/admin.hpp
@@ -0,0 +1,27 @@
+#ifndef admin_hpp
+#define admin_hpp
+
+class Admin {
+    private :
+        string admin_id;
+        string admin_pwd;
+        string admin_mail;
+    public :
+        Admin();
+        void login();
+        void signup();
+};
+
+Admin::Admin() {
+
+}
+
+void Admin::login() {
+
+}
+
+void Admin::signup() {
+
+}
+
+#endif
